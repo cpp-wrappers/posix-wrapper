814ba23dd090b380f022d64650fc2f89d9edcab7
fewizz <hazeevaidar@gmail.com>
2022-10-06 01:30:40 +0300

    fix file access modes for mingw64

--- include/posix/__io/file_access_mode.hpp
+++ include/posix/__io/file_access_mode.hpp
@@ -6,12 +6,15 @@ namespace posix {
 
 enum class file_access_mode {
 	#ifdef __MINGW64__
-	read = 0x0000,
-	write = 0x0001,
+	read       = 0x0000,
+	write      = 0x0001,
 	read_write = 0x0002,
-	append = 0x0008,
-	create = 0x0100,
-	exclusive = 0x0200
+	append     = 0x0008,
+	create     = 0x0100,
+	trunc      = 0x0200,
+	exclusive  = 0x0400,
+	text       = 0x4000,
+	binary     = 0x8000
 	#else
 	static_assert(false);
 	#endif
